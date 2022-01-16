bfdc7ff501d004c129e82cde2611db04233e86f2
Zoe <buckzoe5@gmail.com>
2022-01-16 18:08:10 -0500

    commit tree d3ee0119d7064572f9422fb1c1253f592a2dfa8d

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
-// Write your code here
+// Create a variable called multiple set to an equation that will multiply the variables
+// num1 and num2; the result of the multiplication should be 62
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+
+// Create a variable called random that will generate a random integer greater than 0
+let random = Math.floor(Math.random() * 10) + 1;
+
+// Create a variable called mod to set to an equation that will calculate the remainder of 
+// dividing variavble num3 by num4; the remainder should be 4
+let num3 = 8782;
+let num4 = 42;
+let mod = num3 % num4;
+
+// Create a variable called max that finds the highest number in a set; the value returned should be 20
+let max = Math.max(5, 10, 15, 20);
